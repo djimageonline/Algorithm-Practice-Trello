7169d05ce42e6c5b4b73ca04f6d026b72ca69967
Johnny Proano <jproano22@yahoo.com>
2023-02-02 21:36:33 -0500

    completed reversed string in Ruby

---
+++ reverse_string.rb
@@ -0,0 +1,30 @@
+
+# C V W D R
+
+# Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+#     v  
+# abcde 
+
+# reverse = []
+# index = string.length - 1
+# index > 0 we want to add that to the new string.
+# return the reverse_string
+
+
+def reverse_string(string)
+  reverse = ""
+  index = string.length - 1
+  while index >= 0
+    reverse << string[index]
+    index -= 1
+  end
+  return reverse
+end
+
+p reverse_string("water")
+
+
