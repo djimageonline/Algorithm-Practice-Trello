d3e43a5c3f423d4c484f5e5fe5b892691bd5151e
Johnny Proano <jproano22@yahoo.com>
2023-02-03 12:11:16 -0500

    intial start to show_money problem - unifisnished

---
+++ show_money.rb
@@ -0,0 +1,37 @@
+
+# C V W D R
+
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
+
+
+#           v   
+# “abcdefghi$jklmnopqrstuvwxyz”
+
+
+# index = 0
+# string[index]
+# conditional: if string[index] === $ then its true otherwise false.
+# while loop iterates throught the loop and increments the index by 1 each time.
+# return if true or if false.
+
+def show_me_money(string)
+  index = 0
+  $ = "$"
+  while index < string.length
+    if string[index] == "$"
+      return "true"
+    else
+      return "false"
+    end
+    index += 1
+  end
+end
+
+p show_me_money("i hate $ but i love money i know i know im crazy")
+
